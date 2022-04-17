5b4332a733cf9c78f3869b84122121cab849afed
mattyjtx <mattgonzales3@yahoo.com>
2022-04-16 20:01:24 -0500

    working on fetching weather api

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 var currentList = document.querySelector('ul');
 var searchButton = document.getElementById('.button');
 
+var cityName = document.getElementsByClassName('name');
+
 function getApi() {
-  // replace `octocat` with anyone else's GitHub username
-  var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=754f38cbb3735138d910d50a9de8ed09';
+  
+  var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=a2eb597d271dbead6179a99d9d6c31da";
 
   fetch(requestUrl)
   .then(function (response) {
