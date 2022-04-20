09b682a721e6ea2696b094f620579f6ae9d5c21c
mattyjtx <mattgonzales3@yahoo.com>
2022-04-19 20:53:42 -0500

    updated js file

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
-var currentList = document.querySelector('ul');
-var searchButton = document.getElementById('.button');
+var currentList = document.querySelector("ul");
+var searchButton = document.getElementById("searchBtn");
+var showCityEl = document.querySelector(".city");
 
-var cityName = document.getElementsByClassName('name');
+var cityNameEl = document.getElementById("cityName");
 
 function getApi() {
+  var cityName = cityNameEl.value;
+
+  var url1 =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    cityName +
+    "&units=imperial&appid=a2eb597d271dbead6179a99d9d6c31da";
+
   
-  var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=a2eb597d271dbead6179a99d9d6c31da";
 
-  fetch(requestUrl)
-  .then(function (response) {
+  fetch(url1)
+    .then(function (response) {
       return response.json();
-  })
-  .then(function (data) {
-      console.log(data);
-  })
-};
+    })
+    .then(function (data) {
+      showCityEl.textContent = data.name;
 
+      var url2 = "https://api.openweathermap.org/data/2.5/onecall?lat="+ data.coord.lat +"&lon="+ data.coord.lon +"&appid=a2eb597d271dbead6179a99d9d6c31da&units=imperial"
 
-getApi();
+      fetch(url2)
+        .then(function (response) {
+
+          return response.json();
+
+        })
+        .then(function (data) {
+
+          console.log(data); // data back from last API
+          // everything else goes in here
+
+        });
+    });
+}
+
+searchButton.addEventListener("click", getApi);
